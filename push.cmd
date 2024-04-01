ade7fda850a8b6ccc604ffa04282ea60b83afe21
Lunatixz <128316+Lunatixz@users.noreply.github.com>
2024-03-31 21:17:20 -0400

    PseudoTV Bumpers

---
+++ push.cmd
@@ -0,0 +1,6 @@
+C:
+cd\Program Files\TeraCopy
+TeraCopy.exe Copy "C:\GitHub\PseudoTV_Resources\resource.images.pseudotv.logos" C:\Program Files\Kodi\portable_data\addons\resource.images.pseudotv.logos /OverwriteAll
+cd\GitHub\\PseudoTV_Resources
+addon_generator_del_old.zips.py
+
